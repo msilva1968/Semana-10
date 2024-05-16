b3fe799f214610ddb43d2b68a46e426b99635952
Marcio Silva <marcio.silva.ter@estacio.br>
2024-05-16 09:46:28 -0300

    Ajustes

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });  
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
